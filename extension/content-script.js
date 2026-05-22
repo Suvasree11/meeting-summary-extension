ba8c2acf3455eb1ac9babe4efeec79d7db9dc6f8
CODECLOUDS-SUVASREE <suvasree.official@gmail.com>
2026-05-23 00:18:05 +0530

    new

--- extension/content-script.js
+++ extension/content-script.js
@@ -4,7 +4,6 @@ const EXT_SOURCE = "meetmind-extension";
 function postToPage(message) {
   window.postMessage({ source: EXT_SOURCE, ...message }, "*");
 }
-
 function extensionUnavailableMessage(error) {
   const text = error instanceof Error ? error.message : String(error ?? "");
   if (text.includes("Extension context invalidated")) {
